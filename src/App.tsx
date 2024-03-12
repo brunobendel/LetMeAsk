aac2d2589edd9a8ff70154857086815e8358bded
brunobendel <75317932+brunobendel@users.noreply.github.com>
2024-03-12 18:09:54 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from 'react-router-dom';
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
+export function Home() {
+    const navigate = useNavigate();
+
+    function navigateToNewRoom() {
+        navigate('/rooms/new');
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="ilustração perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
+                <p>Tire as duvidas da sua audiencia em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="logo letmeasc" />
+                    <button onClick={navigateToNewRoom} className='create-room'>
+                        <img src={googleIconImg} alt="" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className='separator'>
+                        Ou entre em uma sala
+                    </div>
+                    <form>
+                        <input type="text"
+                            placeholder='Digite o código da sala' />
+                        <Button type='submit'>
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
