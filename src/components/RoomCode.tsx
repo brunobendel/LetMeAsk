f43f8e9052129fc8d5c3ad6d0fd3ea881a4e1c27
brunobendel <75317932+brunobendel@users.noreply.github.com>
2024-04-02 17:58:51 -0300

    usestate

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
 import { useState } from "react";
-=======
-import React, { useState } from 'react';
->>>>>>> b0f94cef38fab79d854ca020ee82596431a4ce5c
 import copyImg from "../assets/images/copy.svg";
 import "../styles/room-code.scss";
 
@@ -14,20 +10,13 @@ type RoomCodeProps = {
 export function RoomCode({ code = "" }: RoomCodeProps) {
     const [copied, setCopied] = useState(false);
 
-    const [copied , setCopied]= useState(false);
-
-
     function copyRoomCodeToClipboard() {
         navigator.clipboard.writeText(code);
         setCopied(true);
-<<<<<<< HEAD
         setTimeout(()=>{
             setCopied(false);
 
         },1000)
-=======
-        setTimeout(() => setCopied(false), 3000); // Defina um timer para redefinir o estado após 3 segundos
->>>>>>> b0f94cef38fab79d854ca020ee82596431a4ce5c
     }
 
     return (
@@ -35,16 +24,11 @@ export function RoomCode({ code = "" }: RoomCodeProps) {
             <div>
                 <img src={copyImg} alt="Copy Room Code" />
             </div>
-<<<<<<< HEAD
             {copied ? (
                 <span>Copiado com sucesso!</span>
             ):(
                 <span>Sala #{code}</span>
             )}
-=======
-            <span>Sala #{code}</span>
-            {copied && <span style={{ marginLeft: '5px' }}>Copiado!</span>}
->>>>>>> b0f94cef38fab79d854ca020ee82596431a4ce5c
         </button>
     );
 }
